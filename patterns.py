6ed831ecb0011290af0b31b34790748def094ddc
abbouddome <edabboud02@outlook.com>
2022-02-02 12:04:59 +0000

    Added generate_pattern function

--- patterns.py
+++ patterns.py
@@ -21,11 +21,27 @@ class Pattern:
             self.pattern = self.generate_pattern(data, word)
 
     def generate_pattern_from_turn(self, str_status, guess):
-
+        """Returns pattern by comparing turn result to guess"""
         return [(STR_TO_STATUS[str], letter) for str, letter in zip(str_status, guess)]
 
-    def generate_pattern(self):
-        pass
+    def generate_pattern(self, word, guess):
+        """Returns pattern by comparing a word to guess"""
+        pattern = []
+        greens = defaultdict(int)
+        for a_letter, b_letter in zip(word, guess):
+            if a_letter == b_letter:
+                greens[b_letter] += 1
+
+        occurences = defaultdict(int)
+        for a_letter, b_letter in zip(word, guess):
+            if a_letter == b_letter:
+                pattern.append(TILE.GREEN, b_letter)
+            else:
+                tile = TILE.YELLOW if b_letter in word and occurences[b_letter] < word.count(b_letter) - greens(b_letter) else TILE.GREY
+                pattern.append(tile, b_letter)
+                occurences[b_letter] += 1
+        
+        return pattern 
 
     def readable_pattern(self):
         """Returns the string version of the pattern"""
